2b29fb9bde51f293a916c0a78587c9d26224799b
JackRyanPL <34188659+JackRyanPL@users.noreply.github.com>
2017-12-02 17:01:26 +0100

    Create deszyfrowanie.c

---
+++ deszyfrowanie.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+	while(1>0)
+	{
+		char znak;
+		scanf("%c", &znak);
+		
+		if(znak=='W') printf("Q");
+		if(znak=='E') printf("W");
+		if(znak=='R') printf("E");
+		if(znak=='T') printf("R");
+		if(znak=='Y') printf("T");
+		if(znak=='U') printf("Y");
+		if(znak=='I') printf("U");
+		if(znak=='O') printf("I");
+		if(znak=='P') printf("O");
+		if(znak=='A') printf("P");
+		if(znak=='S') printf("A");
+		if(znak=='D') printf("S");
+		if(znak=='F') printf("D");
+		if(znak=='G') printf("F");
+		if(znak=='H') printf("G");
+		if(znak=='J') printf("H");
+		if(znak=='K') printf("J");
+		if(znak=='L') printf("K");
+		if(znak=='Z') printf("L");
+		if(znak=='X') printf("Z");
+		if(znak=='C') printf("X");
+		if(znak=='V') printf("C");
+		if(znak=='B') printf("V");
+		if(znak=='N') printf("B");
+		if(znak=='M') printf("N");
+		if(znak=='Q') printf("M");
+		
+		if(znak==' ') printf(" ");
+		
+		if(znak=='w') printf("q");
+		if(znak=='e') printf("w");
+		if(znak=='r') printf("e");
+		if(znak=='t') printf("r");
+		if(znak=='y') printf("t");
+		if(znak=='u') printf("y");
+		if(znak=='i') printf("u");
+		if(znak=='o') printf("i");
+		if(znak=='p') printf("o");
+		if(znak=='a') printf("p");
+		if(znak=='s') printf("a");
+		if(znak=='d') printf("s");
+		if(znak=='f') printf("d");
+		if(znak=='g') printf("f");
+		if(znak=='h') printf("g");
+		if(znak=='j') printf("h");
+		if(znak=='k') printf("j");
+		if(znak=='l') printf("k");
+		if(znak=='z') printf("l");
+		if(znak=='x') printf("z");
+		if(znak=='c') printf("x");
+		if(znak=='v') printf("c");
+		if(znak=='b') printf("v");
+		if(znak=='n') printf("b");
+		if(znak=='m') printf("n");
+		if(znak=='q') printf("m");
+	}
+}
